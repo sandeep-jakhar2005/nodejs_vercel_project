73b34d30600299c4a3e0a6ef471415549ce5a117
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-01 18:36:40 +0530

    new updated

--- api/home.js
+++ api/home.js
@@ -1,22 +1,24 @@
-const http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    const url = req.url;
-
-    if (url === '/about') {
-        res.write(' Welcome to about us page');
-        res.end();
-    }
-    else if (url === '/contact') {
-        res.write(' Welcome to contact us page');
-        res.end();
-    }
+export default function handler(req, res) {
+    console.log(req.method);
+    if (req.method === 'GET') {
+      // Handle GET request
+        res.status(200).json({ message: 'GET: All products listed' });
+    } 
+    else if (req.method === 'POST') {
+      // Handle POST request
+        res.status(201).json({ message: 'POST: Product created' });
+    } 
+    else if (req.method === 'PUT') {
+      // Handle PUT request
+        res.status(200).json({ message: `PUT: Product  updated`});
+    } 
+    else if (req.method === 'DELETE') {
+      // Handle DELETE request
+        res.status(200).json({ message: `DELETE: Product  deleted` });
+    } 
     else {
-        res.write('Hello World!');
-        res.end();
+      // Method Not Allowed
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
-}).listen(3000, function () {
-    console.log("server start at port 3000");
-});
+  }
+  
