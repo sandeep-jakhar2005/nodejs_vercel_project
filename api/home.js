7876f7bdea241f44ae6c26006208c81af72a7f9f
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-01 18:32:50 +0530

    new route updates

--- api/home.js
+++ api/home.js
@@ -1,23 +1,22 @@
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-      // Handle GET request
-      res.status(200).json({ message: 'GET: All products listed' });
-    } 
-    else if (req.method === 'POST') {
-      // Handle POST request
-      res.status(201).json({ message: 'POST: Product created' });
-    } 
-    else if (req.method === 'PUT') {
-      // Handle PUT request
-      res.status(200).json({ message: `PUT: Product  updated`});
-    } 
-    else if (req.method === 'DELETE') {
-      // Handle DELETE request
-      res.status(200).json({ message: `DELETE: Product  deleted` });
-    } 
-    else {
-      // Method Not Allowed
-      res.status(405).json({ error: 'Method Not Allowed' });
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const url = req.url;
+
+    if (url === '/about') {
+        res.write(' Welcome to about us page');
+        res.end();
     }
-  }
-  
+    else if (url === '/contact') {
+        res.write(' Welcome to contact us page');
+        res.end();
+    }
+    else {
+        res.write('Hello World!');
+        res.end();
+    }
+}).listen(3000, function () {
+    console.log("server start at port 3000");
+});
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-return response('successfully vercal project working');
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const url = req.url;
+
+    if (url === '/about') {
+        res.write(' Welcome to about us page');
+        res.end();
+    }
+    else if (url === '/contact') {
+        res.write(' Welcome to contact us page');
+        res.end();
+    }
+    else {
+        res.write('Hello World!');
+        res.end();
+    }
+}).listen(3000, function () {
+    console.log("server start at port 3000");
+});
