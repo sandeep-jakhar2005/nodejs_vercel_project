e88fb309c57f96da4e63b8c0ea5f1a566ef4c2a8
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-02 10:54:17 +0530

    add token api

---
+++ api/token.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const apiUrl = `https://api.unifiedfitnessplatform.ai/token?refresh_token`;
+
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+            });
+
+            const contentType = response.headers.get('content-type');
+
+            if (contentType && contentType.includes('application/json')) {
+                const data = await response.json();
+                res.status(response.status).json(data);
+            } else {
+                const errorText = await response.text();
+                res.status(response.status).send({
+                    error: 'API did not return JSON',
+                    response: errorText
+                });
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Something went wrong', details: error.message });
+        }
+    }else{
+        res.status(200).json({ message: 'Method not supported' });
+    }
+}
