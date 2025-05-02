f133b70fc61155126848cca9fe68cbc9f7e3d1ea
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-02 11:26:37 +0530

    srwertfd

--- api/token.js
+++ api/token.js
@@ -5,14 +5,11 @@ let tokenExpiryTime = null;  // Token ki expiry time ko store karne ke liye
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         try {
-            // Step 1: Check if we have a cached token and if it's expired
             if (cachedToken && tokenExpiryTime && Date.now() < tokenExpiryTime) {
-                // Token is still valid, use the cached token
                 console.log("Using cached token");
-                return res.status(200).json({ token: cachedToken, Message: 'Reuse Token'  });  // Or use it in your API request
+                return res.status(200).json({ cachedToken: cachedToken, Message: 'Reuse Token'  }); 
             }
 
-            // Step 2: If no valid cached token, fetch a new token
             const apiUrl = `https://api.unifiedfitnessplatform.ai/token`;
 
             const response = await fetch(apiUrl, {
@@ -20,20 +17,18 @@ export default async function handler(req, res) {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(req.body),  // Assuming you are passing body from client
+                body: JSON.stringify(req.body),
             });
 
             const contentType = response.headers.get('content-type');
             if (contentType && contentType.includes('application/json')) {
                 const data = await response.json();
-                const { token, expires_in } = data;  // Assuming the API response contains these keys
+                const { token, expires_in } = data;
 
-                // Step 3: Cache the token and its expiry time
                 cachedToken = token;
-                tokenExpiryTime = Date.now() + expires_in * 1000;  // Convert expires_in from seconds to milliseconds
+                tokenExpiryTime = 1746525015 * 1000; ;
 
-                // Send the token response
-                res.status(response.status).json({ token5: cachedToken, expires_in: expires_in });
+                res.status(response.status).json({ token: cachedToken, expires_in: expires_in });
             } else {
                 const errorText = await response.text();
                 res.status(response.status).send({
