4de6fc268d3e7be68ac8e9cbab44d6d0a7950ac7
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-12 18:10:30 +0530

    fgfhfghfgh

--- api/client-check-in.js
+++ api/client-check-in.js
@@ -69,17 +69,7 @@ export default async function handler(req, res) {
                 const date = new Date(input);
                 // Check for invalid date
                 if (isNaN(date.getTime())) {
-                            const saudiTime = new Date().toLocaleString("en-US", { 
-                            timeZone: "Asia/Riyadh",
-                            year: 'numeric',
-                            month: '2-digit',
-                            day: '2-digit',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit',
-                            hour12: false 
-                        });
-                        date = new Date(saudiTime);
+                        date = new Date();
                         throw new Error(date);
                 }
                 // if (isNaN(date.getTime())) {
