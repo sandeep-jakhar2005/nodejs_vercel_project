d6f06678656c299dc86b08588d4ee8913401f9ad
sandeep-jakhar <sandeep@mindwebtree.com>
2025-05-12 18:16:14 +0530

    dvvdv

--- api/client-check-in.js
+++ api/client-check-in.js
@@ -130,12 +130,18 @@ export default async function handler(req, res) {
                 // }
 
                 // Format manually
+                // const formatted = date.getFullYear() + '-' +
+                // String(date.getMonth() + 1).padStart(2, '0') + '-' +
+                // String(date.getDate()).padStart(2, '0') + ' ' +
+                // String(date.getHours()).padStart(2, '0') + ':' +
+                // String(date.getMinutes()).padStart(2, '0') + ':' +
+                // String(date.getSeconds()).padStart(2, '0');
                 const formatted = date.getFullYear() + '-' +
                 String(date.getMonth() + 1).padStart(2, '0') + '-' +
-                String(date.getDate()).padStart(2, '0') + ' ' +
+                String(date.getDate()).padStart(2, '0') + 'T' +
                 String(date.getHours()).padStart(2, '0') + ':' +
                 String(date.getMinutes()).padStart(2, '0') + ':' +
-                String(date.getSeconds()).padStart(2, '0');
+                String(date.getSeconds()).padStart(2, '0') + 'Z';
 
                 if (response.ok) {
                     checkinTime = data.checkin_date_time || "";
